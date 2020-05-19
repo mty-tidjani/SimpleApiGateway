b3427c8bbeede0ddbd3d32b13942e23e24c7bc9d
Tidjani <mty.tidjani@gmail.com>
2020-05-19 22:36:42 +0100

    Writting the jwt middleware for test

--- core/middlewares/auth.ts
+++ core/middlewares/auth.ts
@@ -1,11 +1,15 @@
 
 class AuthMiddleware {
-  static ware = (_req: any, res: any, next: () => void) => {
+  public static ware = (_req: any, res: any, next: () => void) => {
     // Todo add some logic
     // console.log('You must be auttenticated to pass');
     next();
   }
+
+  public static test = () => {
+
+  }
 }
 
 
-export default AuthMiddleware.ware;
+export default AuthMiddleware;
