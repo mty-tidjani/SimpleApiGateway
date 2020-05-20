a34bfd0e7568cd7404bcea77f8bc6bb700f5c7f6
Tidjani <mty.tidjani@gmail.com>
2020-05-20 02:31:38 +0100

    Adding file logging to increase the quality

--- core/controller/authController.ts
+++ core/controller/authController.ts
@@ -13,10 +13,14 @@ class AuthController {
   public login = async (req: any, res: any, next: (arg0: Error) => void) => {
     try {
       const resp = Queries.post(`${this.config.authApi}/login`, req.body);
+
       const token = makeToken(resp, this.config.jwtSecret, this.config.jwtEpire);
+
       return res.status(200).json({ token });
+
     } catch (err) {
       const error = new TheError(err.message, err.status);
+
       return next(error);
     }
   }
